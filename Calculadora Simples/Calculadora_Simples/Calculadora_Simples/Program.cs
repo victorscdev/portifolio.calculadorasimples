b6b7a25116b0235cadd408692944f60da70e39cc
Victor Santana Costa <39810026+viictoorcoostah@users.noreply.github.com>
2018-06-05 18:29:45 -0300

    Add files via upload

---
+++ Calculadora Simples/Calculadora_Simples/Calculadora_Simples/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace Aula0010_Mini_Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aprendendo a fazer um calculadora simples
+
+            double d;
+            //d = 12 + 3;//soma
+            //d = 12 - 3;//subtração
+            //d = 12 * 3;//multiplicação (x)
+            //d = 12 / 3;//divisão (/)
+            //Console.WriteLine(d);
+
+            double num1, num2;
+            string tmp;
+
+            Console.WriteLine("Digite um numero: ");
+            tmp = Console.ReadLine();
+            num1 = int.Parse(tmp);
+
+            Console.WriteLine("Digite mais um numero: ");
+            tmp = Console.ReadLine();
+            num2 = int.Parse(tmp);
+
+            double resultado;
+            resultado = num1 + num2;
+            Console.WriteLine("Soma: " + resultado);
+
+            resultado = num1 - num2;
+            Console.WriteLine("Subtração: " + resultado);
+
+            resultado = num1 * num2;
+            Console.WriteLine("Multiplicação: " + resultado);
+
+            resultado = num1 / num2;
+            Console.WriteLine("Divisão: " + resultado);
+
+            Console.ReadKey();
